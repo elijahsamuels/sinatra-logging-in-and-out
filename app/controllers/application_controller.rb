5747ecd74188bcc81014c58e2d375e268a9b5183
elijahsamuels <elijahsamuels@gmail.com>
2020-12-30 09:57:32 -0700

    commit tree 69c0b2db3428776ac1c92b629d9220830b79f8dc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,15 +11,34 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
+    erb :error
   end
-
+  
   get '/account' do
-
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+      erb :account
+    else
+      erb :error
+    end
   end
+    # @user = User.find_by(username: params[:username])
+    #   if @user
+    #       erb :account
+    #   else
+    #     erb :error
+    # end
+    # if the user is logged in, show their name and balance
+  
 
   get '/logout' do
-
+    session.clear
+    redirect to '/'
   end
 
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,22 @@
 class Helpers
+
+    # returns the current user
+    # should accept the session hash as an argument. This method should use the user_id from the session hash to find the user in the database and return that user
+    def self.current_user(session)
+        @user = User.find_by_id(session[:user_id])
+        # @user_id = User.find_by(user_id: params[:user_id]) # this doesnt work here.
+    end  
+
+    
+    # should also accept the session hash as an argument. This method should return true if the user_id is in the session hash and false if not
+    def self.is_logged_in?(session)
+        !!session[:user_id]
+        #  # this works, but the above is faster
+        # if @user == User.find_by_id(session[:user_id]) : true || false
+        #     true
+        # else
+        #     false
+        # end
+    end
+
 end
